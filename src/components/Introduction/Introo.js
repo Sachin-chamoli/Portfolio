3e73538b2ae345dd62b5d7b14e7e0c5dbe802951
Sachin-chamoli <79299103+Sachin-chamoli@users.noreply.github.com>
2023-04-03 19:13:32 +0530

    added auto typing title

--- src/components/Introduction/Introo.js
+++ src/components/Introduction/Introo.js
@@ -10,20 +10,59 @@ const Introo = () => {
       duration: 1300,
     });
   },[])
+
+  useEffect(()=>{
+    const text = document.querySelector(".sec-text");
+ const textLoad = () =>{
+    setTimeout(() => {
+      text.textContent = "Frontend Development";
+      console.log("check")
+    }, 0);
+    setTimeout(() => {
+      text.textContent = "Book Reading";
+    }, 7000);
+    setTimeout(() => {
+      text.textContent = "Pencil Sketching";
+    }, 14000);
+  }
+  textLoad()
+  setInterval(textLoad, 21000);
+
+  },[])
+  // const textLoad = () =>{
+  //   setTimeout(() => {
+  //     text.textContent = "Frontend Development";
+  //     console.log("check")
+  //   }, 0);
+  //   setTimeout(() => {
+  //     text.textContent = "Book Reading";
+  //   }, 6000);
+  //   setTimeout(() => {
+  //     text.textContent = "Pencil Sketching";
+  //   }, 12000);
+  // }
+  // textLoad()
+  // setInterval(textLoad, 18000);
+
+  
   return (
     <div id="I" className='i'>
       <div data-aos="fade-up" className="i-left">
         <div className="i-left-wrapper">
             <h2 className='i-intro'>Hello, My name is</h2>
             <h1 className='i-name'>Sachin Chamoli</h1>
-            <div className="i-title">
-              <div className="i-title-wrapper">
+            {/* <div className="i-title"> */}
+              {/* <div className="i-title-wrapper">
                 <div className="i-title-item">Web Developer</div>
                 <div className="i-title-item">Sketch Artist</div>
                 <div className="i-title-item">Photographer</div>
                 <div className="i-title-item">Teacher</div>
+              </div> */}
+              <div className="i-title">
+                <span className="text first-text">Interested in : </span>
+                <span className="text sec-text">Frontend Developer</span>
               </div>
-            </div>
+            {/* </div> */}
             <p className="i-desc">
               This is my portfolio that represent what I learn What I do and some of my recent projects .
             </p>
